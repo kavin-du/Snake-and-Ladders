fe6bc9a71df5b4ba7c6c35f78da8e7a8f4849639
kavin-du <ukdkcm@gmail.com>
2021-03-12 22:18:59 +0530

    comments added

--- program.py
+++ program.py
@@ -1,7 +1,21 @@
-import turtle
-import random
+import turtle # import turtle graphics
+import random # import random for generating random numbers
 
-widthOfSquare = 120
+"""
+Basic Game Development in Python
+---------------------------------
+This is a Snakes and Ladders game that allows two players to
+participate in turns. 
+
+Developed by: ____
+Date Created: ____
+
+"""
+
+# keeping these unchanged variables as global for easyness
+widthOfSquare = 120 
+# cordinates for the cow and bull, each tuple contains x, y cordinates values 
+# for all the 25 squares
 cordinatesList = [(-250, -230), (-130, -230), (-10, -230), (110, -230), (230, -230),
                   (230, -110), (110, -110), (-10, -110), (-130, -110), (-250, -110),
                   (-250, 10), (-130, 10), (-10, 10), (110, 10), (230, 10),
@@ -9,11 +23,18 @@ cordinatesList = [(-250, -230), (-130, -230), (-10, -230), (110, -230), (230, -2
                   (-250, 250), (-130, 250), (-10, 250), (110, 250), (230, 250),]
 
 def setupGrid():
-    t = turtle.Turtle(visible=False)
-    t.pen(pencolor="blue", pensize=3, speed=10)
-    t.penup()
+    """ Function for setting up grid layout """
+    t = turtle.Turtle(visible=False) 
+    
+    # changing color, width and speed of line
+    t.pen(pencolor="blue", pensize=3, speed=10) 
+    t.penup() # move without drawing
+
+    # move to bottom left corner
     t.goto(-5*widthOfSquare/2, -5*widthOfSquare/2)
-    t.pendown()
+    t.pendown() # start drawing again
+
+    # below for loops draws the grid lines in the turtle window
     for _ in range(4):
         t.forward(5*widthOfSquare)
         t.left(90)
@@ -31,100 +52,141 @@ def setupGrid():
         t.forward(5*widthOfSquare)
 
 def boxNumbers():
-    t = turtle.Turtle(visible=False)
-    t.pen(pencolor="red", speed=10)
-    t.penup()
+    """ Function for printing box numbers in the grid window """
+    t = turtle.Turtle(visible=False) # hide the turtle pointer
+    t.pen(pencolor="red", speed=10) # changing color and speed
+    t.penup() # hide the drawings
+    
+    # this part will print numbers 1 to 5 on the grid
     xCord = -(widthOfSquare*5/2-5)
     for i in range(5):
         t.goto(xCord, -210)
-        t.write(str(i+1), font=('', 18, 'italic')) # set the default system font, check this on windows 10
+        t.write(str(i+1), font=('', 18, 'italic')) # set the default system font of OS
         xCord += widthOfSquare
     
+    # this part will print numbers 6 to 10 on the grid
     t.left(90)
     t.forward(widthOfSquare)
     t.left(90)
     for i in range(6, 11):
-        t.write(str(i), font=('', 18, 'italic')) # set the default system font, check this on windows 10
-        t.forward(widthOfSquare)
+        t.write(str(i), font=('', 18, 'italic')) # set the default system font of OS
+        t.forward(widthOfSquare) # move to next square
     
+    # this part will print numbers 11 to 15 on the grid
     t.right(90)
     t.forward(widthOfSquare)
     t.right(90)
     t.forward(widthOfSquare)
     for i in range(11, 16):
-        t.write(str(i), font=('', 18, 'italic')) # set the default system font, check this on windows 10
-        t.forward(widthOfSquare)
+        t.write(str(i), font=('', 18, 'italic')) # set the default system font of OS
+        t.forward(widthOfSquare) # move to next square
 
+    # this part will print numbers 16 to 20 on the grid
     t.left(90)
     t.forward(widthOfSquare)
     t.left(90)
     t.forward(widthOfSquare)
     for i in range(16, 21):
-        t.write(str(i), font=('', 18, 'italic')) # set the default system font, check this on windows 10
-        t.forward(widthOfSquare)
+        t.write(str(i), font=('', 18, 'italic')) # set the default system font of OS
+        t.forward(widthOfSquare) # move to next square
 
+    # this part will print numbers 21 to 25 on the grid
     t.right(90)
     t.forward(widthOfSquare)
     t.right(90)
     t.forward(widthOfSquare)
     for i in range(21, 26):
-        t.write(str(i), font=('', 18, 'italic')) # set the default system font, check this on windows 10
-        t.forward(widthOfSquare)
+        t.write(str(i), font=('', 18, 'italic')) # set the default system font of OS
+        t.forward(widthOfSquare) # move to next square
 
 def setImages():
-    t1 = turtle.Turtle()
-    t1.shape("ladder.gif")
-    t1.penup()
-    t1.goto(-100, -50)
+    """ Function for setting up ladders and snakes on the game window """
 
+    # ladder of squares 9-12
+    t1 = turtle.Turtle()
+    t1.shape("ladder.gif") # add the image to turtle pointer
+    t1.penup() # hide the moving path
+    t1.goto(-100, -50) # postion in the correct place
+
+    # ladder of squares 18-23
     t2 = turtle.Turtle()
-    t2.shape("ladder2.gif")
-    t2.penup()
+    t2.shape("ladder2.gif") # add the image to turtle pointer
+    t2.penup() # hide the moving path
     t2.goto(0, 190)
 
+    # ladder of squares 5-15
     t3 = turtle.Turtle()
-    t3.shape("ladder3.gif")
-    t3.penup()
+    t3.shape("ladder3.gif") # add the image to turtle pointer
+    t3.penup() # hide the moving path
     t3.goto(250, -140)
 
+    # snake of squares 14-24
     t4 = turtle.Turtle()
-    t4.shape("snake.gif")
-    t4.penup()
+    t4.shape("snake.gif") # add the image to turtle pointer
+    t4.penup() # hide the moving path
     t4.goto(120, 120)
 
+    # snake of squares 3-8
     t5 = turtle.Turtle()
-    t5.shape("snake2.gif")
-    t5.penup()
+    t5.shape("snake2.gif") # add the image to turtle pointer
+    t5.penup() # hide the moving path
     t5.goto(0, -170)
 
+    # snake of squares 1-20
     t6 = turtle.Turtle()
-    t6.shape("snake3.gif")
-    t6.penup()
+    t6.shape("snake3.gif") # add the image to turtle pointer
+    t6.penup() # hide the moving path
     t6.goto(-240, -50)
 
 def setCowAndBull(cow, bull):
-    cow.penup()
-    cow.goto(-250, -230)
+    """ Function for setting up cow and bull in the initial position """
+    
+    cow.penup() # hide the moving path
+    cow.goto(-250, -230) # position in the correct place
 
-    bull.penup()
-    bull.goto(-250, -270)
+    bull.penup() # hide the moving path
+    bull.goto(-250, -270) # position in the correct place
     
 def moveCow(cow, cordinates):
+    """ 
+    Function for moving the cow to given cordinates. 
+    cordinates are passed as a tuple of (x, y)
+    cow object is also passed as a parameter
+    """
     cow.goto(cordinates[0], cordinates[1])
 
 def moveBull(bull, cordinates):
+    """ 
+    Function for moving the bull to given cordinates. 
+    cordinates are passed as a tuple of (x, y)
+    """
+
+    # the bull is positioned 40 units below the cow
+    # along y axis
     bull.goto(cordinates[0], cordinates[1]-40)
     
 def startGame():
+    """ 
+    Function for keep the game running until user interrupts by keyboard
+    """
+
+    # keeping track of the squares of cow and bull
+    # intially they are at 1st square
     cowIndex = 1
     bullIndex = 1
 
     print('New Game!\n')
-
+    
+    # keep prompting user to press enter
     while(True):
+        # get the Enter press to dummy variable
         dummy = input('Big Bad Bull: Press Enter to roll dice')
+        
+        # generating a random value between 1-6 for the bull
         bullDiceValue = random.randint(1, 6)
-        print(f'bull dice value: {bullDiceValue}')
+        print(f'bull dice value: {bullDiceValue}') # printing the dice value
+        
+        # logic for moving between corner squares
         if(bullIndex < 5 and bullIndex+bullDiceValue >= 6):
             moveBull(bull, cordinatesList[4])
             moveBull(bull, cordinatesList[5])
@@ -137,12 +199,18 @@ def startGame():
         elif(bullIndex <= 20 and bullIndex+bullDiceValue >= 21):
             moveBull(bull, cordinatesList[19])
             moveBull(bull, cordinatesList[20])
-
+        
+        # update the square value of bull
         bullIndex += bullDiceValue
+
+        # reset suquare value to 25 if it is larger
         if(bullIndex > 25):
             bullIndex = 25
+        
+        # move the bull to correct position
         moveBull(bull, cordinatesList[bullIndex-1])
         
+        # logic for checking stairs and snakes
         if(bullIndex == 5): # stairs
             moveBull(bull, cordinatesList[14])
             bullIndex = 15
@@ -164,18 +232,24 @@ def startGame():
 
         print(f'You are on square {bullIndex}\n')
 
+        # check if bull has come to the end
         if (bullIndex == 25):
             print('Bull Wins')
-            turtle.clearscreen()
-            turtle.bgpic("win.gif")
-            dummy = input('Press Enter to start new game')
-            turtle.clearscreen()
-            return
+            turtle.clearscreen() # clear everything in window
+            turtle.bgpic("win.gif") # show win image
+            dummy = input('Press Enter to start new game') # wait to press Enter
+            turtle.clearscreen() # clear win image
+            return # return to main function
+        
         # ------------------
+        # getting user input for cow and store in a dummy variable
         dummy = input('Fluffy Cow: Press Enter to roll dice')
+        # generating dice value for cow
         cowDiceValue = random.randint(1, 6)
+        # printing dice value
         print(f'cow dice value: {cowDiceValue}')
 
+        # logic for move between corner squares
         if(cowIndex < 5 and cowIndex+cowDiceValue >= 6):
             moveCow(cow, cordinatesList[4])
             moveCow(cow, cordinatesList[5])
@@ -189,11 +263,17 @@ def startGame():
             moveCow(cow, cordinatesList[19])
             moveCow(cow, cordinatesList[20])
 
+        # update the square value of cow
         cowIndex += cowDiceValue
+
+        # if index is larger than 25, reset it to 25
         if(cowIndex > 25):
             cowIndex = 25
+        
+        # move the cow to correct position
         moveCow(cow, cordinatesList[cowIndex-1])
 
+        # logic for checking stairs and snakes
         if(cowIndex == 5): # stairs
             moveCow(cow, cordinatesList[14])
             cowIndex = 15
@@ -215,18 +295,21 @@ def startGame():
 
         print(f'You are on square {cowIndex}\n')
 
+        # check if cow has come to end
         if(cowIndex == 25):
             print('Cow Wins')
-            turtle.clearscreen()
-            turtle.bgpic("win.gif")
-            dummy = input('Press Enter to start new game')
-            turtle.clearscreen()
-            return
+            turtle.clearscreen() # clear everything
+            turtle.bgpic("win.gif") # show win image
+            dummy = input('Press Enter to start new game') # wait to press Enter
+            turtle.clearscreen() # clear win image
+            return # return to main function
 
 if __name__ == '__main__':
     
-    turtle.title('Python Turtle Graphics')
-    turtle.setup(700, 700, 0, 0)
+    turtle.title('Python Turtle Graphics') # set title of window
+    turtle.setup(700, 700, 0, 0) # this is a 700 x 700 window with starting position (0,0)
+
+    # adding .gif images to program
     turtle.addshape("ladder.gif")
     turtle.addshape("ladder2.gif")
     turtle.addshape("ladder3.gif")
@@ -237,22 +320,24 @@ if __name__ == '__main__':
     turtle.addshape("cow.gif")
     turtle.addshape("win.gif")
 
+    # the game runs infinitely until user interrupts by keyboard
     while(True):
-        setupGrid()
-        boxNumbers()
-        setImages()
-
+        setupGrid() # painting box
+        boxNumbers() # painting box numbers
+        setImages() # adding images
+        
+        # creating cow figure
         cow = turtle.Turtle()
         cow.shape("cow.gif")
         
+        # creating bull figure
         bull = turtle.Turtle()
         bull.shape("bull.gif")
 
+        # move cow and bull to intial positions
         setCowAndBull(cow, bull)
-        startGame()
+        startGame() # run the game
 
-    # turtle.clearscreen()
-    # turtle.bgpic("win.gif")
 
-    turtle.exitonclick()
+    turtle.exitonclick() # keep the window until user terminates
 
